6c57385f16f0dcf0417d92a147358c2d4f9045e3
ohad92 <73739848+ohad92@users.noreply.github.com>
2021-07-10 23:14:21 +0300

    league info

--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -2,7 +2,8 @@ const axios = require("axios");
 const DButils = require("./DButils");
 const team_utils = require("./team_utils")
 const games_utils = require("./games_utils")
-const LEAGUE_ID = "271";
+const LEAGUE_ID = 271;
+const STAGE_ID = 18334;
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 
 async function getLeagueDetails() {
@@ -15,14 +16,14 @@ async function getLeagueDetails() {
       },
     }
   );
-  const stage = await axios.get(
-    `${api_domain}/stages/${league.data.data.current_stage_id}`,
-    {
-      params: {
-        api_token: process.env.api_token,
-      },
-    }
-  );
+  // const stage = await axios.get(
+  //   `${api_domain}/stages/season/${STAGE_ID}`,
+  //   {
+  //     params: {
+  //       api_token: process.env.api_token,
+  //     },
+  //   }
+  // );
 
   let closesetGame = await games_utils.NextGameInLeague();
   //console.log(closesetGame[0].homeTeamId);
@@ -32,7 +33,7 @@ async function getLeagueDetails() {
   return {
     league_name: league.data.data.name,
     current_season_name: league.data.data.season.data.name,
-    current_stage_name: stage.data.data.name,
+    // current_stage_name: stage.data,
     // next game details should come from DB
     next_game_date: closesetGame[0].date,
     next_game_hometeam: hometeam,
